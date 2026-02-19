cb483247e5684662bdb5f4750fc72aad56d2d20e
HeitorPF <heitorpf12.hf@gmail.com>
2026-02-19 14:37:52 -0300

    não mostrar diretamente "no vocabulary data found"

--- frontend/src/components/KanjiVocab.jsx
+++ frontend/src/components/KanjiVocab.jsx
@@ -7,21 +7,24 @@ export function KanjiVocab({ vocab }) {
         <ul className='kanji-vocab-list'>
           {vocab.data.map((word) => {
             return (
-                <li className='kanji-vocab-word' key={word.japanese}>
-                  <p>{word.japanese}</p>
-                  <p>{word.meaning.english}</p>
-                </li>
+              <li className='kanji-vocab-word' key={word.japanese}>
+                <p>{word.japanese}</p>
+                <p>{word.meaning.english}</p>
+              </li>
             )
           })}
         </ul>
       </div>
     )
   }
-  else{
-    return(
-      <div>
-        No vocabulary data found
-      </div>
-    )
+  else {
+    if (vocab.query && !vocab.data) {
+      return (
+        <div>
+          No vocabulary data found
+        </div>
+      )
+    }
+
   }
 }
